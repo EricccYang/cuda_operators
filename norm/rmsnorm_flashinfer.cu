0e6623b89c8eca2d0b45f3954075e43d4c00006e
root <EricccYang>
2026-01-07 16:23:48 +0800

    rms

--- norm/rmsnorm_flashinfer.cu
+++ norm/rmsnorm_flashinfer.cu
@@ -5,6 +5,7 @@
 
 #define BLOCK_SIZE 512
 #define DATA_PER_THREAD 4
+#define WARP_SIZE 32
 
 
 //512个数一个emb， 一个线程处理4个数，4个warp一个emb，先这么写
@@ -13,22 +14,23 @@
 __global__  void RMSNorm(float* g_idata, unsigned int n, unsigned int d){
     const int warp_size = 32;
     int tx= threadIdx.x;
-    int tid = threadIdx.y * blockIdx.x + tx;
+    int ty = threadIdx.y;
+    int tid = threadIdx.y * blockDim.x + tx;
 
-    int threads_count = blockDim.x;
+    int threads_count = blockDim.x * blockDim.y;
     int datas_per_block = threads_count * DATA_PER_THREAD;
-    int* block_begin_data_ptr = g_idata +  blockIdx.x * datas_per_block;
-    int* warp_begin_data_ptr = block_begin_data_ptr + ty * warp_size* DATA_PER_THREAD;
+    float* block_begin_data_ptr = g_idata +  blockIdx.y * datas_per_block;
+    float* warp_begin_data_ptr = block_begin_data_ptr + ty * warp_size* DATA_PER_THREAD;
 
     
-    float sum = 0.0;
+    float sum = 0.0f;
 
     #pragma unroll
     for(int i =0 ;i < DATA_PER_THREAD; i++){
         sum += warp_begin_data_ptr[ tx + i * warp_size] * warp_begin_data_ptr[ tx + i * warp_size];
     }
     
-    extern __shared__ float sm[];
+
     //一个warp把管理的 warp_size* DATA_PER_THREAD 的数据加完，分散在warp_size个线程里
     //然后shuf
     sum+=__shfl_down_sync(0xffffffff,sum,16);
@@ -38,24 +40,28 @@ __global__  void RMSNorm(float* g_idata, unsigned int n, unsigned int d){
     sum+=__shfl_down_sync(0xffffffff,sum,1);
     
 
+    __shared__ float sm[16];
     //然后每个warp存一个sm的位置
     //用每个warp的第一个线程 目前是
-    if(threadIdx.x == 0){
-        sm[threadIdx.y] = sum;
+    if(tx == 0){
+        sm[ty] = sum;
     }
     __syncthreads();
 
     //用一个warp规约sm      其实也能shuf
+
+    
     for(int stride =   blockDim.y/2 ; stride > 0 ; stride/=2){
         //用第一个warp的threads就可以
         if(tid < stride){
             sm[tid] += sm[tid+ stride];
         }
+       
     }
     __syncthreads();
+    
 
-
-    float reduced =  sqrtf( sm[0] / blockDim.x * DATA_PER_THREAD*  blockDim.x);
+    float reduced =  sqrtf( sm[0] / (blockDim.x * DATA_PER_THREAD*  blockDim.y));
     
 
     //把自己负责的数字读出来再除以这个值再放回去
@@ -179,9 +185,9 @@ int main(){
     cudaMemcpy(d_B, h_B, nBytes, cudaMemcpyHostToDevice);
 
 
-    dim3 block( WARP_SIZE * DATA_PER_THREAD , block_size/(WARP_SIZE * DATA_PER_THREAD)  );
+    dim3 block( WARP_SIZE , block_size/(WARP_SIZE * DATA_PER_THREAD)  );
     int grid_size = n/ block_size;
-    dim3 grid(1, grid_size );
+    dim3 grid(1, grid_size);
 
 
     // 先用CPU计算参考结果
@@ -189,14 +195,14 @@ int main(){
     RMSNormCPU(h_A, hostRef, n, block_size);
     printf("CPU reference computed.\n");
 
-    RMSNorm<<<grid,block>>>(d_B , n);
+    RMSNorm<<<grid,block>>>(d_B , n, block_size);
 
     // 运行GPU核函数
     cudaEvent_t start, end;
     cudaEventCreate(&start);
     cudaEventCreate(&end);
     cudaEventRecord(start);
-    RMSNorm<<<grid,block>>>(d_A , n);
+    RMSNorm<<<grid,block>>>(d_A , n, block_size);
     cudaEventRecord(end);
     cudaEventSynchronize(end);
     float msec, sec;
@@ -204,7 +210,8 @@ int main(){
     sec = msec / 1000.0;
     
     printf("\n=== GPU Execution ===\n");
-    printf("Execution configuration <<<%d, %d>>>\n", grid.x, block.x);
+    printf("Execution configuration grid<%d, %d>\n", grid.x, grid.y);
+    printf("Execution configuration block<%d, %d>\n", block.x, block.y);
     printf("Execution time: %f ms (%f s)\n", msec, sec);
 
     // 将GPU结果拷回主机
