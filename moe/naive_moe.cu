062eceaa36e44b5e1512f31944d34b91873a8fe0
EricccYang <yangyang4991@gmail.com>
2026-05-31 16:31:18 +0800

    ad

--- moe/naive_moe.cu
+++ moe/naive_moe.cu
@@ -85,7 +85,8 @@ __global__ void select_topk(float* logits, int num_tokens, int num_experts, int*
 void init_logits(float* logits, int num_tokens, int num_experts) {
     for (int t = 0; t < num_tokens; ++t) {
         for (int e = 0; e < num_experts; ++e) {
-            logits[t * num_experts + e] = (float)(t * 13 + e) * 0.01f;
+            // 让每个 token 的起始值不同，每个 expert 连续递增
+            logits[t * num_experts + e] = 0.01f * (t * num_experts + e);
         }
     }
 }
